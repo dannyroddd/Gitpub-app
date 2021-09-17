f5fe39f76a849af4abca3a7dc7019c3e4a720d03
Daniel Rodriguez <danielrodriguez@Daniels-MBP.home>
2021-09-17 16:04:02 -0400

    Displays drinks objects with /drinks route

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 
+const drinks = require('./models/drinks')
+
 app.get('/', (req, res)=>{
     res.send('Welcome to the Gitpub App!')
 })
+
+app.get('/drinks', (req, res) => {
+    res.send(drinks);
+});
 
 
 app.listen(3000, () => {
