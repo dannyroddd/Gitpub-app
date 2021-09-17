0e71533cd69c9b2317ada2d3e508cd1d15a3e9d0
Daniel Rodriguez <danielrodriguez@Daniels-MBP.home>
2021-09-17 16:53:53 -0400

    set up show route in server.js

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/drinks', (req, res) => {
     res.render('index.ejs', {allDrinks: drinks})
 });
 
+app.get('/drinks/:id', (req, res) => {
+    res.send(req.params.id)
+  });
+
 
 app.listen(3000, () => {
     console.log('listening');
