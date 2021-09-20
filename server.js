fb8acbc5f240f5d9715cdb41da3b23f6218dd1f4
Daniel Rodriguez <danielrodriguez@Daniels-MBP.home>
2021-09-20 00:27:57 -0400

    Adds food menu to index and shows each food in its own show page

--- server.js
+++ server.js
@@ -2,19 +2,23 @@ const express = require('express');
 const app = express();
 
 const drinks = require('./models/drinks')
+const food = require('./models/food')
 
 app.get('/', (req, res)=>{
     res.send('Welcome to the Gitpub App!')
 })
 
 app.get('/drinks', (req, res) => {
-    res.render('index.ejs', {allDrinks: drinks})
+    res.render('index.ejs', {allDrinks: drinks, allFood: food})
 });
 
 app.get('/drinks/:id', (req, res) => {
-    res.render("drinkshow.ejs", {drink: drinks[req.params.id]});
-  });
+    res.render("drinkshow.ejs", {drink: drinks[req.params.id]})  
+});
 
+app.get('/food/:id', (req, res) => {
+    res.render("foodshow.ejs", {foods: food[req.params.id]})  
+});
 
 app.listen(3000, () => {
     console.log('listening');
